9331e34f543ca6c85e279834b753ead14ab9625c
Ben Kozuch <benkozuch@Bens-MBP.attlocal.net>
2022-11-22 14:00:58 -0600

    boilerplate with added input field

---
+++ dapp-frontend/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {ethers} from 'ethers';
+    import {abi, address} from './contractData.js';
+
+    let connectedAccount = "";
+    let contractMessage = "asdfadf";
+    let new_message = "";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    async function connectWallet() {
+        if (window.ethereum) {
+            await window.ethereum.request({method: 'eth_requestAccounts'})
+            const signer = provider.getSigner();
+            connectedAccount = await signer.getAddress();
+        } else {
+            alert('You need to install metamask');
+        }
+    }
+
+    async function callContract() {
+        const contract = new ethers.Contract(address, abi, provider);
+        contractMessage = await contract.message();
+    }
+
+    async function setMessage() {
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(address, abi, signer);
+        await contract.setMessage(new_message);
+    }
+</script>
+
+
+<main>
+    <div>Connected Account: {connectedAccount}</div>
+    <div>Contract Message: { contractMessage }</div>
+
+    <button on:click={connectWallet}>Connect wallet</button>
+    <button on:click={callContract}>Call Contract</button>
+    <button on:click={setMessage}>Set Message</button>
+    <input type="text"  bind:value={new_message}>
+</main>
---
+++ dapp-frontend/src/contractData.js
@@ -0,0 +1,35 @@
+export const abi = [
+    {
+        inputs: [],
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+    {
+        inputs: [],
+        name: "message",
+        outputs: [
+            {
+                internalType: "string",
+                name: "",
+                type: "string",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+        constant: true,
+    },
+    {
+        inputs: [
+            {
+                internalType: "string",
+                name: "_message",
+                type: "string",
+            },
+        ],
+        name: "setMessage",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+];
+export const address = "0x152A73F786aa61D6453B4736575c5F5D7F887913";
